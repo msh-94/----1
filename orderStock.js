948ebbc1f15c24f01f26508618f36d7104a069d9
yeonwooshin1 <tlswnsgur456@gmail.com>
2025-06-29 05:09:53 +0900

    잡니다

--- orderStock.js
+++ orderStock.js
@@ -25,6 +25,9 @@ function setBoard (){
     }
 }
 
+const DAMAGE_RATE = 3;   // 파손  3%
+
+
 
 function boardEdit(){
 
@@ -37,7 +40,7 @@ function boardEdit(){
     const quantity = document.querySelector('#quantity');
     const reason = document.querySelector('#reason');
 
-    const quantityV = quantity.value;
+    const quantityV = quantity.value.trim();
     const reasonV = reason.value.trim();
 
     const error = [];
@@ -50,17 +53,39 @@ function boardEdit(){
         return;
     }
 
+    const qtyTotal = Number(quantityV);
+
+    let normalCnt = 0; let damageCnt = 0;
+    
+    for(let k = 0; k < qtyTotal ; k++){
+
+        const ramdom = Math.random() * 100; // Math.ramdom * 100 은 0~100까지의 난수를 만들어라임
+
+        if(ramdom < DAMAGE_RATE ){
+            damageCnt++
+        }
+        else{ normalCnt++ }
+        //  카운터의 합은 qtyTotal 과 반드시 같음!
+    }
+
 
     for(let i = 0; i < productList.length; i++){
         const pArray = productList[i] ;
         
         if(selectno == pArray.pno){
-            pArray.pAmount += Number(quantityV);
+            pArray.pAmount += qtyTotal;
 
             let logco = inoutLog.length === 0 ? 1 : inoutLog[inoutLog.length -1 ].logco + 1;
-
+            
             inoutLog.push({ logco , pno : Number(selectno) , inOut : '입고' ,
-            pName : pArray.pName , amount : Number(quantityV) , area : reasonV , date : dateV  });
+            pName : pArray.pName , amount : normalCnt , area : `${reasonV} ( 정상품 )` , date : dateV  });
+            
+            logco++;
+
+            if(damageCnt > 0){
+                inoutLog.push({ logco : logco++ , pno : Number(selectno) , inOut : '입고' ,
+                pName : pArray.pName , amount : damageCnt , area : `${reasonV} ( 파손품 )` , date : dateV  });
+            }
             
             localStorage.setItem('productList' , JSON.stringify(productList));
                                                                                      
@@ -73,7 +98,7 @@ function boardEdit(){
         window.opener.stockList?.();
         window.opener.logListAdd?.(); }
 
-    alert('입고 처리가 완료되었습니다!');
+     alert(`입고 완료!\n - 정상 : ${normalCnt}\n - 파손 : ${damageCnt}`);
     window.close();                      // 팝업이라면 닫아 주기   
 }
 
