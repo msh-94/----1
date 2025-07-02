e27e6085b3f05f67e6c0c504f1149ab30d74413a
KaFelLa92 <choidj10@gmail.com>
2025-07-02 14:12:32 +0900

    ㅁㄴㅇㄹ

--- dongjin/practice.js
+++ dongjin/practice.js
@@ -1,4 +1,9 @@
-// 특정 차트 컨테이너만 열리게 하는 함수
+// 1. 특정 차트 컨테이너만 열리게 하는 함수
 function showOnlyContainer(targetId) {
+
+    // 1) 모든 차트 컨테이너는 기본으로 닫힌 상태
     document.querySelector('#ChartContainer').style.display = 'none';
+    document.querySelector('#branchChartContainer').style.display = 'none';
+
+    // 
 }
