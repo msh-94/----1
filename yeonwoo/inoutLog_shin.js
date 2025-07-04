459348be6a6b5c9342b814633b24e83e4b89aa60
yeonwooshin1 <tlswnsgur456@gmail.com>
2025-07-04 10:50:02 +0900

    디버깅수정

--- yeonwoo/inoutLog_shin.js
+++ yeonwoo/inoutLog_shin.js
@@ -7,7 +7,7 @@ const m = String(now.getMonth()+1).padStart(2,'0');
 const d = String(now.getDate()).padStart(2,'0'); 
 
 const currentDate = `${y}-${m}-${d}`;
-
+  
 //================================================================//
 
 
